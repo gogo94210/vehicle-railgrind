696707ff84b7695b3c926c1aebe816ed99c709b0
gogo94210 <augustin.cropsal@epita.fr>
2024-08-06 18:02:41 +0200

    tech research element for car grind and research icon

---
+++ data.lua
@@ -0,0 +1,20 @@
+local carGrindTech = {
+    type = "technology",
+    name = "car-grind",
+    icon = "__vehicle-railgrind__/car_tech.png"
+    icon_size = 128,
+    prerequisites = {"automobilism", "railway"},
+    unit =
+    {
+        count = 200,
+        ingredients =
+        {
+            {"automation-science-pack", 1},
+            {"logistic-science-pack", 1}
+        },
+        time = 30
+    },
+    order = "c-a"
+}
+
+data:extend{carGrindTech}
