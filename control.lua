b93fcb732902fbbffa42115ffeb9a1991a9774fa
gogo94210 <augustin.cropsal@epita.fr>
2024-08-07 01:04:59 +0200

    dispaly the entitiy the player is standing on

--- control.lua
+++ control.lua
@@ -4,7 +4,15 @@ script.on_event(defines.events.on_player_changed_position, function(event) Playe
 
 function PlayerMoved(event)
     local player = game.get_player(event.player_index)
-    local player_pos = {x = round(player.position.x), y = round(player.position.y)}
-    local player_tile = player.surface.get_tile(player_pos)
-    player.print(string.format("Currently sitting on a %s.", player_tile.name))
+    local found = player.surface.find_entities_filtered{position = player.position}
+    for k,v in pairs(found) do
+        player.print(v.name)
+    end
+    -- player.print(string.format("Entity below me is a %s.", found))
+
+    -- interesting but gets the ground type, not the entity
+    -- the round function is defined in the utils and I did this because the get_tile takes a TilePosition which is two ints and MapPosition is two doubles
+    -- local player_pos = {x = round(player.position.x), y = round(player.position.y)}
+    -- local player_tile = player.surface.get_tile(player_pos)
+    -- player.print(string.format("Currently sitting on a %s.", player_tile.name))
 end
--- utils.lua
+++ utils.lua
@@ -2,4 +2,3 @@
 function round(num)
   return num + (2^52 + 2^51) - (2^52 + 2^51)
 end
-
