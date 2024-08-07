af5b0da0834b99570118c6807c73851853629c9b
gogo94210 <augustin.cropsal@epita.fr>
2024-08-07 15:26:16 +0200

    print rail orientation

--- control.lua
+++ control.lua
@@ -4,11 +4,12 @@ script.on_event(defines.events.on_player_changed_position, function(event) Playe
 
 function PlayerMoved(event)
     local player = game.get_player(event.player_index)
-    local found = player.surface.find_entities_filtered{position = player.position}
-    for k,v in pairs(found) do
-        player.print(v.name)
+    if (player.driving) then
+        local rails = player.surface.find_entities_filtered{position = player.position, name = {"straight-rail", "curved-rail"}}
+        for k,v in pairs(rails) do
+            player.print(string.format("This is a %s with an orientation of %s", v.name, v.orientation))
+        end
     end
-    -- player.print(string.format("Entity below me is a %s.", found))
 
     -- interesting but gets the ground type, not the entity
     -- the round function is defined in the utils and I did this because the get_tile takes a TilePosition which is two ints and MapPosition is two doubles
