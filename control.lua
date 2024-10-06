ca4c856009057be8e24493ac4bfd0ea6ba15239e
gogo94210 <augustin.cropsal@epita.fr>
2024-10-06 22:47:51 +0200

    debug info for testing on rails

--- control.lua
+++ control.lua
@@ -2,14 +2,28 @@ require "utils"
 
 script.on_event(defines.events.on_player_changed_position, function(event) PlayerMoved(event) end)
 
+
+
 function PlayerMoved(event)
     local player = game.get_player(event.player_index)
     if (player.driving) then
+        local car_orientation = player.vehicle.orientation
+
         local rails = player.surface.find_entities_filtered{position = player.position, name = {"straight-rail", "curved-rail"}}
+        local rail_orientation = 0
+        local rail_name = ""
+
         for k,v in pairs(rails) do
-            player.print(string.format("This is a %s with an orientation of %s", v.name, v.orientation))
+            rail_orientation = v.orientation
+            rail_name = v.name
+            -- player.print(string.format("This is a %s with an orientation of %s", v.name, v.orientation))
+        end
+
+        if (rail_name == "straight-rail") then
+            player.vehicle.orientation = aligned_angle_straight(car_orientation, rail_orientation)
         end
     end
+
 
     -- interesting but gets the ground type, not the entity
     -- the round function is defined in the utils and I did this because the get_tile takes a TilePosition which is two ints and MapPosition is two doubles
--- utils.lua
+++ utils.lua
@@ -1,4 +1,32 @@
+-- CONSTANTS
+
+NORTH   = 0
+NE      = 0.125
+EAST    = 0.25
+SE      = 0.375
+SOUTH   = 0.5
+SW      = 0.625
+WEST    = 0.75
+NW      = 0.875
+
 -- https://stackoverflow.com/a/58411671
 function round(num)
   return num + (2^52 + 2^51) - (2^52 + 2^51)
+end
+
+function approx(real, wanted, margin)
+    real = (real * 1000) % 1000
+    return 
+end
+
+function aligned_angle_straight(car_orientation, rail_orientation)
+    if (rail_orientation == NORTH) then
+        return 
+    elseif (rail_orientation == NE) then
+        return
+    elseif (rail_orientation == EAST) then
+        return
+    else -- south east
+        return
+    end
 end
